48f5669fa8692f7ed6b41631cb8c68d9bfdc6e88
Pitttike <gabor.wittmann.uzlet@gmail.com>
2024-11-26 01:14:15 +0100

    fet: create error handling

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,15 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 import { UpdateEmailDto } from './dto/update-email.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class EmailService {
-  db: PrismaService;
-  constructor(db: PrismaService) {
-    this.db = db;
+  errors: string[];
+
+  constructor(private db: PrismaService) {
   }
   create(createEmailDto: CreateEmailDto) {
+    
+    const { sender, receiver, subject, content } = createEmailDto;
+    
+    //validate createEmailDto
+    this.errors = [];
+
+    if (!sender || !receiver || !subject || !content) {
+      this.errors.push('Az adatok nem lehetnek üresek!');
+    }
+
+    if (sender === receiver) {
+      this.errors.push('A küldő és a címzett nem lehet ugyanaz!');
+    }
+
+    if (subject.length < 3) {
+      this.errors.push('A tárgynak minimum 3 karakternek kell lennie!');
+    }
+
+    if (content.length < 10) {
+      this.errors.push('A tartalomnak minimum 10 karakternek kell lennie!');
+    }
+
+    if (!RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(sender) || !RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(receiver)) {
+      this.errors.push('A küldő és a címzettnek email címnek kell lennie!');
+    }
+
+    if (this.errors.length > 0) {
+      throw new HttpException(this.errors, HttpStatus.BAD_REQUEST);
+    }
+
     return this.db.email.create({
       data: createEmailDto
     })
